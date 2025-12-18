e2876083fe1f4c81edac60f80dd95f4057cd2314
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-12-18 21:03:56 +0200

    js/lesson-3-redoing.js

--- js/lesson-3-redoing.js
+++ js/lesson-3-redoing.js
@@ -115,3 +115,162 @@ const countSum = numbers =>
   }, 0);
 
 console.log(countSum(numbers4));
+
+// ------ 11
+
+class Calculator {
+  currentValue;
+
+  number(value) {
+    this.currentValue = value;
+    return this;
+  }
+
+  getResult() {
+    return this.currentValue;
+  }
+
+  add(value) {
+    this.currentValue += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.currentValue -= value;
+    return this;
+  }
+
+  divide(value) {
+    value !== 0
+      ? (this.currentValue /= value)
+      : alert('Sorry! Not correct operation!');
+
+    return this;
+  }
+
+  multiply(value) {
+    this.currentValue *= value;
+    return this;
+  }
+
+  reset() {
+    this.currentValue = 0;
+    return this;
+  }
+}
+
+const calc = new Calculator();
+
+const result = calc
+  .number(10)
+  .add(5)
+  .subtract(3)
+  .multiply(4)
+  .divide(2)
+  .getResult();
+
+console.log(result); // 24
+
+const calc2 = new Calculator();
+
+const result2 = calc2.number(24).add(37).multiply(56).getResult();
+
+console.log(result2); // 3416
+
+// ----- 12
+
+class Client {
+  #login;
+  #email;
+
+  get login() {
+    return this.#login;
+  }
+
+  set login(newLogin) {
+    this.#login = newLogin;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  set email(newEmail) {
+    newEmail.includes('@')
+      ? (this.#email = newEmail)
+      : alert('Incorrect email!');
+  }
+}
+
+const newClient = new Client();
+
+console.log(Client);
+console.log(newClient);
+
+newClient.login = 'bestlogin';
+console.log(newClient.login);
+
+newClient.email = 'something@gmail.com';
+console.log(newClient.email);
+
+console.log(newClient);
+
+// ----- 13
+
+const personData = {
+  name: 'Kevin',
+  age: 32,
+  gender: 'male',
+  email: 'kevin@gmail.com',
+};
+
+class Person {
+  constructor({ name, age, gender, email }) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.email = email;
+  }
+
+  getDetails() {
+    return {
+      name: this.name,
+      age: this.age,
+      gender: this.gender,
+      email: this.email,
+    };
+  }
+}
+
+const newPerson = new Person(personData);
+console.log(newPerson.getDetails());
+
+const employeeData = {
+  name: 'Ben',
+  age: 48,
+  gender: 'male',
+  email: 'ben@gmail.com',
+  salary: 34000,
+  department: 'hr',
+};
+
+class Employee extends Person {
+  constructor({ name, age, gender, email, salary, department }) {
+    super({ name, age, gender, email });
+    this.salary = salary;
+    this.department = department;
+  }
+
+  getEmployeeDetails() {
+    return { salary: this.salary, department: this.department };
+  }
+}
+
+const newEmployee = new Employee(employeeData);
+
+console.log(newEmployee);
+
+console.log({
+  ...newEmployee.getDetails(),
+  ...newEmployee.getEmployeeDetails(),
+});
